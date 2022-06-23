5a93d47da0c53998d8c5915d02f13ef1c8122cdf
Sam Williams <sam@thirdwave.tv>
2022-06-23 16:36:44 -0400

    Updated script.js to invert if logic to get the correct behavior

--- script.js
+++ script.js
@@ -3,9 +3,9 @@ var names = ["Yaakov", "John", "Jen", "Jason", "Paul", "Frank", "Larry", "Paula"
 for (i = 0; i < names.length; i++) {
 	var startingChar = names[i][0].toLowerCase();
 	if(startingChar == 'j'){
-		helloSpeaker.speak(names[i]);
+		byeSpeaker.speak(names[i]);
 	}
 	else{
-			window.byeSpeaker.speak(names[i])
+			helloSpeaker.speak(names[i])
 	}
 }
