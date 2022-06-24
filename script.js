7dbe76faa933cbdfe4a4b41a93520acd2e52b6a1
Sam Williams <sam@thirdwave.tv>
2022-06-24 09:10:57 -0400

    Updated to include script.js in an IIFE

--- script.js
+++ script.js
@@ -1,11 +1,12 @@
-var names = ["Yaakov", "John", "Jen", "Jason", "Paul", "Frank", "Larry", "Paula", "Laura", "Jim"];
-
-for (i = 0; i < names.length; i++) {
-	var startingChar = names[i][0].toLowerCase();
-	if(startingChar == 'j'){
-		byeSpeaker.speak(names[i]);
-	}
-	else{
-			helloSpeaker.speak(names[i])
-	}
+(function (){
+	var names = ["Yaakov", "John", "Jen", "Jason", "Paul", "Frank", "Larry", "Paula", "Laura", "Jim"];
+	for (i = 0; i < names.length; i++) {
+		var startingChar = names[i][0].toLowerCase();
+		if(startingChar == 'j'){
+			byeSpeaker.speak(names[i]);
+		}
+		else{
+				helloSpeaker.speak(names[i])
+		}
 }
+})();
